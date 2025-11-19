ec21204464b780a854bbe7d295924082d43eb0a1
Michael Brooke <michaelbrooke93@gmail.com>
2025-11-19 21:21:00 +0100

    Create ListNode

---
+++ src/leetcode/linked/list/ReverseLinkedList.java
@@ -0,0 +1,26 @@
+package leetcode.linked.list;
+
+import dsa.linked.list.ListNode;
+
+/*
+Given the beginning of a singly linked list head,
+reverse the list, and return the new beginning of the list.
+
+Example 1:
+    Input: head = [0,1,2,3]
+    Output: [3,2,1,0]
+
+Example 2:
+    Input: head = []
+    Output: []
+
+Constraints:
+    0 <= The length of the list <= 1000.
+    -1000 <= Node.val <= 1000
+ */
+public class ReverseLinkedList {
+
+    public ListNode reverseList(ListNode head) {
+        return null;
+    }
+}
