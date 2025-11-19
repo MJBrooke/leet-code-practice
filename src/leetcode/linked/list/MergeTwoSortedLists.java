a163279b5236e821c328209ae5a93e363a8d163a
Michael Brooke <michaelbrooke93@gmail.com>
2025-11-19 22:04:09 +0100

    Merge Two Sorted Linked Lists skeleton

---
+++ src/leetcode/linked/list/MergeTwoSortedLists.java
@@ -0,0 +1,37 @@
+package leetcode.linked.list;
+
+import dsa.linked.list.ListNode;
+
+/*
+You are given the heads of two sorted linked lists list1 and list2.
+
+Merge the two lists into one sorted linked list and return the head of the new sorted linked list.
+
+The new list should be made up of nodes from list1 and list2.
+
+Example 1:
+    Input: list1 = [1,2,4], list2 = [1,3,5]
+    Output: [1,1,2,3,4,5]
+
+Example 2:
+    Input: list1 = [], list2 = [1,2]
+    Output: [1,2]
+
+Example 3:
+    Input: list1 = [], list2 = []
+    Output: []
+
+Constraints:
+    0 <= The length of each list <= 100.
+    -100 <= Node.val <= 100
+ */
+public class MergeTwoSortedLists {
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        return null;
+    }
+
+    static void main() {
+
+    }
+}
