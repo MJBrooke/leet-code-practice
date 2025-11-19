b88338d97b89e6b2b77903eb3a669fb32e7c593f
Michael Brooke <michaelbrooke93@gmail.com>
2025-11-19 21:54:40 +0100

    Reverse Linked List

--- src/dsa/linked/list/ListNode.java
+++ src/dsa/linked/list/ListNode.java
@@ -1,9 +1,21 @@
 package dsa.linked.list;
 
 public class ListNode {
-    int val;
-    ListNode next;
-    ListNode() {}
-    ListNode(int val) { this.val = val; }
-    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    public int val;
+    public ListNode next;
+    public ListNode() {}
+    public ListNode(int val) { this.val = val; }
+    public ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+    public void print() {
+        System.out.print("[");
+        System.out.print(val);
+
+        ListNode next = this.next;
+        while(next != null) {
+            System.out.print(", " + next.val);
+            next = next.next;
+        }
+        System.out.println("]");
+    }
 }
--- src/leetcode/linked/list/ReverseLinkedList.java
+++ src/leetcode/linked/list/ReverseLinkedList.java
@@ -20,7 +20,36 @@ Constraints:
  */
 public class ReverseLinkedList {
 
-    public ListNode reverseList(ListNode head) {
-        return null;
+    /*
+    We want to traverse the list.
+    If we keep a pointer to a prev & curr, we can flip the node association.
+    In this question, we aren't tracking head/tail, so that isn't a consideration
+     */
+
+    public static ListNode reverseList(ListNode head) {
+        // If empty, or just one node, no reversal necessary.
+        if (head == null || head.next == null) return head;
+
+        ListNode prev = null; // Initial case has prev as null since it will be the new tail
+        ListNode curr = head; // Start at initial spot
+
+        while (curr != null) { // We iterate curr, so it will be null when the list is done
+            ListNode tmp = curr.next;// Don't lose next, since we are flipping it to prev!
+            curr.next = prev; // Flip association
+
+            // Move our 2 pointers forward
+            prev = curr;
+            curr = tmp;
+        }
+
+        // Prev ends up as the original tail while curr is null at this point
+        return prev;
+    }
+
+    static void main() {
+        var head = new ListNode(0, new ListNode(1, new ListNode(2, new ListNode(3))));
+
+        head.print();
+        reverseList(head).print();
     }
 }
