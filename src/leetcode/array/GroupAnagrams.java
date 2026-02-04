ab27d2b8a8b6d0fe41a25ca72e14ba1ae7d8b4ab
Michael Brooke <michaelbrooke93@gmail.com>
2026-02-04 21:49:58 +0100

    Solve GroupAnagrams using Sort + Map approach

--- src/leetcode/array/GroupAnagrams.java
+++ src/leetcode/array/GroupAnagrams.java
@@ -1,6 +1,6 @@
 package leetcode.array;
 
-import java.util.List;
+import java.util.*;
 
 /*
 Given an array of strings strs, group all anagrams together into sublists.
@@ -28,8 +28,45 @@ Constraints:
  */
 public class GroupAnagrams {
 
+    /*
+    Option 1:
+        For each word, we sort it alphabetically and place it in a HashMap where the key is the sorted word and the value is a list of the actual words.
+        We do this for every word, and then collect up the Values of the HashMap into a list and return it.
+
+        Time complexity:
+            n = number of words
+            k = average word length
+
+            sortWord = k log k (from sorting each word)
+            groupAnagrams = n (we iterate each word once)
+            Final complexity: O(n * k log k)
+        Space complexity:
+            n = number of words
+            k = average word length
+
+            All keys may be unique, so map has n keys
+            Values can each be length k or more
+
+            Final complexity: O(n * k)
+     */
     public static List<List<String>> groupAnagrams(String[] strs) {
-        return null;
+        Map<String, List<String>> anagramGroups = new HashMap<>();
+
+        for (String str : strs) {
+            var sortedStr = sortWord(str);
+
+            var value = anagramGroups.getOrDefault(sortedStr, new ArrayList<>());
+            value.add(str);
+            anagramGroups.put(sortedStr, value);
+        }
+
+        return new ArrayList<>(anagramGroups.values());
+    }
+
+    public static String sortWord(String str) {
+        char[] strArr = str.toCharArray();
+        Arrays.sort(strArr);
+        return new String(strArr);
     }
 
     static void main() {
