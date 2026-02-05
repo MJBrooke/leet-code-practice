e6221c9f62065b3940d735cbb083883ae6bc7b6f
Michael Brooke <michaelbrooke93@gmail.com>
2026-02-05 13:05:36 +0100

    Solve AnagramMappings

--- src/leetcode/array/AnagramMappings.java
+++ src/leetcode/array/AnagramMappings.java
@@ -1,6 +1,6 @@
 package leetcode.array;
 
-import java.util.Arrays;
+import java.util.*;
 
 /*
 You are given two integer arrays nums1 and nums2 where nums2 is an anagram of nums1.
@@ -29,8 +29,44 @@ Constraints:
  */
 public class AnagramMappings {
 
+    /*
+    Option 1:
+        We could double-for loop it.
+        For each entry in nums1, search the entirety of nums2 for a match.
+        Since duplicates exist, we would likely need to track what we have seen to know if we should 'skip' duplicates we have seen.
+        Time complexity will be O(n^2) though, so this solution sucks.
+    Option 2:
+        We could iterate over nums2, creating a HashMap of <NumberValue, List<Index>>.
+        We then iterate over nums1, finding the relevant index, creating the return value as we go.
+
+        Complexity:
+            n1 = length of nums1
+            n2 = length of nums2
+            Time:  O(n1 + n2)
+            Space: O(n1 + n2) since we create a duplicate set of data from nums2 and the array-value
+     */
     public static int[] anagramMappings(int[] nums1, int[] nums2) {
-        return new int[0];
+        // No validation strictly needed due to constraints, but added for completeness
+        if (nums1.length != nums2.length) return new int[0];
+
+        // Build a Map linking the numbers to their indexes (allowing for duplicates)
+        Map<Integer, Deque<Integer>> numberIndexes = new HashMap<>();
+        for (int i = 0; i < nums2.length; i++) {
+            // An ArrayDeque is very fast and efficient if we are only using `removeFirst`
+            var currIndexes = numberIndexes.getOrDefault(nums2[i], new ArrayDeque<>());
+            currIndexes.add(i);
+            numberIndexes.put(nums2[i], currIndexes);
+        }
+
+        //
+        int[] mapping = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            int currNum = nums1[i];
+            int idx = numberIndexes.get(currNum).removeFirst(); // Safe, since we know it is an anagram
+            mapping[i] = idx;
+        }
+
+        return mapping;
     }
 
     static void main() {
