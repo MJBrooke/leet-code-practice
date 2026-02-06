e4d652437eaa7441561f0c9bdcc6202253a21fa0
Michael Brooke <michaelbrooke93@gmail.com>
2026-02-06 12:30:16 +0100

    Solve InsertInterval naively using insert+merge

--- src/leetcode/array/interval/InsertInterval.java
+++ src/leetcode/array/interval/InsertInterval.java
@@ -1,15 +1,17 @@
 package leetcode.array.interval;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /*
-You are given an array of non-overlapping intervals intervals where
+You are given an array of non-overlapping intervals where
 intervals[i] = [start_i, end_i] represents the start and the end time of the ith interval.
-intervals is initially sorted in ascending order by start_i.
+intervals are initially sorted in ascending order by start_i.
 You are given another interval newInterval = [start, end].
 
 Insert newInterval into intervals such that intervals is still sorted in ascending order by start_i
- and also intervals still does not have any overlapping intervals.
+and also intervals still does not have any overlapping intervals.
 You may merge the overlapping intervals if needed.
 Return intervals after adding newInterval.
 
@@ -31,8 +33,75 @@ Constraints:
  */
 public class InsertInterval {
 
+    /*
+    Understanding the problem:
+        Okay, so this time, if I am reading correctly, we can assume the input is already sorted in ascending order by start time.
+        So we do not need to do any initial sort.
+
+        We also already know that all the intervals are already merged (and therefore not overlapping).
+        You can almost assume that the input into this is the output from the `merge` operation in MergeIntervals.
+     */
+
+    /*
+    Option 1:
+        Naively, I would think that we could iterate over the intervals and simply insert the new interval at the correct location.
+        We could then run a `merge` operation again over the full list (but assuming it is already sorted).
+        This would produce a freshly created list of non-overlapping intervals.
+
+        The runtime would be:
+            - O(n) to iterate the list and insert the interval into its sorted position
+            - O(n) to greedy merge the list
+            Total: O(2n) = O(n)
+
+            This of course, assumes that input is already sorted.
+     */
     public static int[][] insert(int[][] intervals, int[] newInterval) {
-        return null;
+        List<int[]> newIntervals = new ArrayList<>();
+
+        // Traverse the intervals, and insert where start_time is still in order
+        boolean inserted = false;
+        for (int[] interval : intervals) {
+            int currStartTime = interval[0];
+            if (!inserted && newInterval[0] <= currStartTime) {
+                newIntervals.add(newInterval);
+                inserted = true;
+            }
+            newIntervals.add(interval);
+        }
+
+        // If intervals is empty, we catch that case and add just the newInterval
+        if (!inserted) newIntervals.add(newInterval);
+
+        // Then merge
+        return merge(newIntervals.toArray(new int[newIntervals.size()][]));
+    }
+
+    public static int[][] merge(int[][] intervals) {
+        List<int[]> mergedIntervals = new ArrayList<>();
+
+        /*
+        Okay, we want to do a normal merge here. So:
+            For every interval in the list:
+                - Check if it should be merged with the adjacent one
+                - if not, add it to our list
+         */
+        int[] left = intervals[0];
+        for (int i = 1; i < intervals.length; i++) {
+            int[] right = intervals[i];
+
+            if (right[0] <= left[1]) {
+                left = new int[]{
+                        left[0],
+                        Math.max(left[1], right[1])
+                };
+            } else {
+                mergedIntervals.add(left);
+                left = right;
+            }
+        }
+        mergedIntervals.add(left);
+
+        return mergedIntervals.toArray(new int[mergedIntervals.size()][]);
     }
 
     static void main() {
