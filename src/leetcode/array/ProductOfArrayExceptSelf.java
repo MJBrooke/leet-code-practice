ea7b6347d0956c2ab85c087fd253f717193eebdf
Michael Brooke <michaelbrooke93@gmail.com>
2026-02-09 17:07:14 +0100

    Solve ProductOfArrayExceptSelf

--- src/leetcode/array/ProductOfArrayExceptSelf.java
+++ src/leetcode/array/ProductOfArrayExceptSelf.java
@@ -23,11 +23,58 @@ Constraints:
     -20 <= nums[i] <= 20
  */
 public class ProductOfArrayExceptSelf {
+
+    /*
+    Option 1 (Brute force):
+        Double-for loop where we multiply all elements except the current element.
+        This will be O(n^2) so not great.
+     */
+
+    /*
+    Option 2:
+        Since we are simply multiplying everything together, we can find the total multiplication value of all elements together.
+        For each index i, we divide that solution by the value at that index to 'remove' it from that total value.
+        We need some careful management of zero values:
+            - No zeroes mean we can just use the total/value-at-index
+            - A single zero means all other indexes other than that zero have a value of zero
+            - Two zeroes mean that everything in the index will be zero
+
+        Complexity:
+            - Time: O(n) since we iterate the length of nums twice. So technically O(2n)
+     */
     public static int[] productExceptSelf(int[] nums) {
-        return new int[0];
+        int[] result = new int[nums.length];
+
+        int total = 1; // Can't be zero, since we are multiplying
+
+        int numZeroes = 0;
+        for (int num : nums) {
+            if (num == 0) {
+                numZeroes++;
+                continue;
+            }
+            total *= num;
+        }
+
+        if (numZeroes >= 2) return result;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 0) // We know 0 does not affect total, so it's just the total as-is
+                result[i] = total;
+            else if (numZeroes > 0) // If we did find a single zero in the input it means everything else will be zero
+                result[i] = 0;
+            else // Otherwise, divide away the value at this index
+                result[i] = total / nums[i];
+        }
+
+        return result;
     }
 
     static void main() {
+        System.out.println(Arrays.toString(
+                productExceptSelf(new int[]{0, 0})
+        )); // Output: [0,0]
+
         System.out.println(Arrays.toString(
                 productExceptSelf(new int[]{1, 2, 4, 6})
         )); // Output: [48,24,12,8]
