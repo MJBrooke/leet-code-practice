040fda564606c0a86bfa416aae789c6fd0664b49
Michael Brooke <michaelbrooke93@gmail.com>
2026-02-12 15:38:19 +0100

    Solve LRU Cache

--- src/leetcode/maps/LRUCache.java
+++ src/leetcode/maps/LRUCache.java
@@ -1,5 +1,7 @@
 package leetcode.maps;
 
+import java.util.*;
+
 /*
 Implement the Least Recently Used (LRU) cache class LRUCache.
 
@@ -46,16 +48,119 @@ Constraints:
  */
 public class LRUCache {
 
-    public LRUCache(int capacity) {
+    /*
+    Understanding the question:
+        We 100% want to use an underlying Java map to keep the key/values stored.
+        The trick here will be adding some way of cleverly tracking which key should be evicted
+            if the capacity is reached.
+        Additionally, this extra structure must keep get and put at O(1) complexity.
+            This means we can't use something like a min-heap.
+        We can use a doubly-linked list, where we can insert/delete in O(1) time.
+            However, the built-in LinkedList does not offer O(1) search/remove to any Node not at Head/Tail.
+            So we will have to build a custom Node-based DLL.
+     */
 
+    private static class Node {
+        int key;
+        int value;
+        Node prev;
+        Node next;
+
+        public Node() {
+            this(0, 0);
+        }
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+    private final int capacity;
+    private final Map<Integer, Node> map;
+
+    /*
+    Important implementation feature (invariant):
+        These are Sentinel nodes that will always be present and never deleted.
+        They are always the head and tail, and are there to simplify our insert/remove functionality.
+        With these, we can always assume a Node's prev and next are non-null values.
+
+        At initialisation:
+            [L] <-> [R]
+        After entries have been added:
+            [L] <-> ... real nodes ... <-> [R]
+     */
+    private final Node leastRecentlyUsedSentinel;
+    private final Node mostRecentlyUsedSentinel;
+
+    public LRUCache(int capacity) {
+        if (capacity <= 0)
+            throw new IllegalArgumentException("Capacity must be > 0");
+
+        this.capacity = capacity;
+        map = new HashMap<>();
+
+        leastRecentlyUsedSentinel = new Node();
+        mostRecentlyUsedSentinel = new Node();
+        leastRecentlyUsedSentinel.next = mostRecentlyUsedSentinel;
+        mostRecentlyUsedSentinel.prev = leastRecentlyUsedSentinel;
+    }
+
+    // Simply link prev and next, removing node from chain.
+    // GC will take care of clean up.
+    // Thanks to sentinel nodes, nulls will never happen
+    private void remove(Node node) {
+        var prev = node.prev;
+        var next = node.next;
+        prev.next = next;
+        next.prev = prev;
+    }
+
+    // Always insert at RHS
+    // Thanks to sentinels, no null checks are needed.
+    // Initial insert is between L & R sentinels, and further are between a real node and R.
+    private void insert(Node node) {
+        var currMostRecent = mostRecentlyUsedSentinel.prev;
+        currMostRecent.next = node;
+        node.prev = currMostRecent;
+        node.next = mostRecentlyUsedSentinel;
+        mostRecentlyUsedSentinel.prev = node;
+    }
+
+    // An existing node can be removed from current location and shifted to end in O(1)
+    private void makeMostRecent(Node node) {
+        remove(node);
+        insert(node);
     }
 
     public int get(int key) {
-        return 0;
+        if (map.containsKey(key)) {
+            var node = map.get(key);
+            makeMostRecent(node);
+            return node.value;
+        }
+        return -1;
     }
 
     public void put(int key, int value) {
+        if (map.containsKey(key)) {
+            // Update key value and make it most recent
+            var node = map.get(key);
+            node.value = value;
+            makeMostRecent(node);
+        } else {
 
+            // If we reach capacity, remove the oldest node (from map and DLL)
+            if (map.size() >= capacity) {
+                map.remove(leastRecentlyUsedSentinel.next.key);
+                remove(leastRecentlyUsedSentinel.next); // Safe since we know Cache is not empty
+            }
+
+            // Insert new node
+            var node = new Node(key, value);
+            insert(node);
+            map.put(key, node);
+        }
     }
 
     static void main() {
