97627e434cf60e7124af66c40c8f59b5af5d62e8
Michael Brooke <michaelbrooke93@gmail.com>
2026-02-15 18:02:36 +0100

    Solve Success Rate Trends

---
+++ src/sql/success_rate_trend.sql
@@ -0,0 +1,29 @@
+/*
+ For Merchant A (M_001), calculate the success rate for February 2025 vs. December 2025
+ Here, the complexity lies in transforming vertical data (rows of transactions) into horizontal results (comparative rates)
+ while handling the "integer division" trap
+ */
+
+ -- Create CTE with rows just representing: date_period (2025-02), is_success (1.0 or 0.0)
+ WITH prepared_data AS (
+     SELECT
+         -- Creates a string from the date, so that we can group on it later
+         SUBSTR(CAST(created_at AS VARCHAR), 1, 7) AS period,
+         -- Add a column of 1 or 0 to be summed together
+         -- We use decimal values for the division later
+         CASE WHEN status = 'SUCCESS' THEN 1.0 ELSE 0.0 END AS is_success
+     FROM transactions
+     WHERE merchant_id = 'M_001'
+ )
+ SELECT
+     period,
+     COUNT(*) AS total_txns,
+     SUM(is_success) AS success_txns,
+     ROUND(
+        -- We catch the case where there are no transactions, causing a divide-by-zero with NULLIF
+        SUM(is_success) / NULLIF(COUNT(*), 0),
+        4 -- Rounding amount
+     ) AS success_rate
+ FROM prepared_data
+ GROUP BY period
+ ORDER BY period;
